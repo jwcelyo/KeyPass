1d6c8e77cb310eb1bd22df6d01f9729ecffc0037
JUCELIO ANCELMO ALENCAR <Dev-Jucelio@live.com>
2019-11-20 19:09:24 -0300

    init version

---
+++ KeyPass/Form1.cs
@@ -0,0 +1,119 @@
+﻿using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KeyPass
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string DecryptData(string EncryptedText, string Encryptionkey)
+        {
+            RijndaelManaged objrij = new RijndaelManaged();
+            objrij.Mode = CipherMode.CBC;
+            objrij.Padding = PaddingMode.PKCS7;
+
+            objrij.KeySize = 0x80;
+            objrij.BlockSize = 0x80;
+            byte[] encryptedTextByte = Convert.FromBase64String(EncryptedText);
+            byte[] passBytes = Encoding.UTF8.GetBytes(Encryptionkey);
+            byte[] EncryptionkeyBytes = new byte[0x10];
+            int len = passBytes.Length;
+            if (len > EncryptionkeyBytes.Length)
+            {
+                len = EncryptionkeyBytes.Length;
+            }
+            Array.Copy(passBytes, EncryptionkeyBytes, len);
+            objrij.Key = EncryptionkeyBytes;
+            objrij.IV = EncryptionkeyBytes;
+            byte[] TextByte = objrij.CreateDecryptor().TransformFinalBlock(encryptedTextByte, 0, encryptedTextByte.Length);
+            return Encoding.UTF8.GetString(TextByte);  //it will return readable string  
+        }
+        string EncryptData(string textData, string Encryptionkey)
+        {
+            RijndaelManaged objrij = new RijndaelManaged();
+            objrij.Mode = CipherMode.CBC;
+            objrij.Padding = PaddingMode.PKCS7;
+            objrij.KeySize = 0x80;
+            objrij.BlockSize = 0x80;
+            byte[] passBytes = Encoding.UTF8.GetBytes(Encryptionkey);
+            byte[] EncryptionkeyBytes = new byte[] { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+
+            int len = passBytes.Length;
+            if (len > EncryptionkeyBytes.Length)
+            {
+                len = EncryptionkeyBytes.Length;
+            }
+            Array.Copy(passBytes, EncryptionkeyBytes, len);
+            objrij.Key = EncryptionkeyBytes;
+            objrij.IV = EncryptionkeyBytes;
+            ICryptoTransform objtransform = objrij.CreateEncryptor();
+            byte[] textDataByte = Encoding.UTF8.GetBytes(textData);
+            return Convert.ToBase64String(objtransform.TransformFinalBlock(textDataByte, 0, textDataByte.Length));
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text.Equals(textBox4.Text))
+            {
+                textBox3.Text = EncryptData(textBox1.Text, textBox2.Text);
+                QRCodeGenerator qrGenerator = new QRCodeGenerator();
+                QRCodeData qrCodeData = qrGenerator.CreateQrCode(textBox3.Text, QRCodeGenerator.ECCLevel.Q);
+                QRCode qrCode = new QRCode(qrCodeData);
+                Bitmap qrCodeImage = qrCode.GetGraphic(20);
+                pictureBox1.Image = qrCodeImage;
+            }
+            else
+            {
+                MessageBox.Show("Password not matched!");
+                textBox2.Text = string.Empty;
+                textBox4.Text = string.Empty;
+                textBox2.Focus();
+            }
+         
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text.Equals(textBox4.Text))
+            {
+                textBox3.Text = DecryptData(textBox1.Text, textBox2.Text);
+                QRCodeGenerator qrGenerator = new QRCodeGenerator();
+                QRCodeData qrCodeData = qrGenerator.CreateQrCode(textBox3.Text, QRCodeGenerator.ECCLevel.Q);
+                QRCode qrCode = new QRCode(qrCodeData);
+                Bitmap qrCodeImage = qrCode.GetGraphic(20);
+                pictureBox1.Image = qrCodeImage;
+            }
+            else
+            {
+                MessageBox.Show("Password not matched!");
+                textBox2.Text = string.Empty;
+                textBox4.Text = string.Empty;
+                textBox2.Focus();
+            }
+            
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://t.me/keypasscrypto");
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            label6_Click(null,null);
+        }
+    }
+}
